9123af29000b108542bd0086b618214dfd1db758
davidcoats <david@getsafetycone.com>
2021-11-01 16:22:18 -0700

    Initial commit of source-stub.

---
+++ source/R5T.D0088.I0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0088.I0001.Construction
+{
+    /// <summary>
+    /// Construction project for A default HostStartupBase implementation for local scripts..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.D0088.I0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0088.I0001
+{
+    /// <summary>
+    /// A default HostStartupBase implementation for local scripts.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
